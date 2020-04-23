3f8c8792fbf80d3e62d3594c46acbb10efc80a48
SquonK <sp@squonk.fr>
2020-04-23 02:05:51 +0200

    push push baby

--- ligue_des_etoiles.py
+++ ligue_des_etoiles.py
@@ -1,11 +1,18 @@
 import re
 import sys
 import pprint as pp
+from collections import OrderedDict
 
 
-
-list_of_players = {}
+list_of_players = OrderedDict()
 list_of_other_names = {}
+
+rules = OrderedDict()
+rules["+ ="] = ("red", "+ = alea")
+rules["mm ma"] = ("orange", "mm ma elem")
+rules["mp + ="] = ("blue", "mp + = alea")
+rules["mp"] = ("darkgreen", "mp bat")
+rules["mkc"] = ("darkyellow", "mkc elem bat")
 
 
 is_reading_players = True
@@ -119,6 +126,7 @@ with open("input.txt", encoding="utf-8") as f:
             list_of_players[names[0]]["games"]["mp"] = {}
             list_of_players[names[0]]["games"]["mkc"] = {}
             list_of_players[names[0]]["name"] = names[0]
+            list_of_players[names[0]]["ranking"] = {}
 
             for name in names:
                 list_of_other_names[name] = names[0]
@@ -130,13 +138,207 @@ with open("input.txt", encoding="utf-8") as f:
 
 
 
+def compute_rank(list_of_games):
+    total_played = 0
+    total_score = 0
 
-print("List of players")
-pp.pprint(list_of_players, indent=2)
+    for opponent in list_of_games:
+        total_played += 1
+        total_score += list_of_games[opponent]
+    
+    d = {}
+    d["played"] = total_played
+    d["score"] = total_score
+    return d
 
 
-print("List of other names")
-pp.pprint(list_of_other_names, indent=2)
+for player_name in list_of_players:
+    m = 9999
+
+    for rule in ["+ =", "mm ma", "mp + =", "mp", "mkc"]:
+        list_of_players[player_name]["ranking"][rule] = compute_rank(list_of_players[player_name]["games"][rule])
+        m = min(m, list_of_players[player_name]["ranking"][rule]["played"])
+    
+    list_of_players[player_name]["played"] = m
+
+
+
+
+
+#print("List of players")
+#pp.pprint(list_of_players, indent=2)
+
+
+#print("List of other names")
+#pp.pprint(list_of_other_names, indent=2)
+
+
+
+print("=======================")
+print("=======================")
+print("=======================")
+print()
+print()
+print('<table class="wikitable" border="1" style="text-align: center;">')
+
+def get_global_header():
+    s = "<tr>"
+    s += "<th>Nom</th><th>Joués</th>"
+
+    for player_name in list_of_players:
+        s += "<th>" + player_name + "</th>"
+
+    s += "</tr>"
+    return s
+
+print(get_global_header())
+
+
+for player_name in list_of_players:
+    p_dict = list_of_players[player_name]
+
+    s = "<tr>"
+    s += "<th>" + player_name + "</th>"
+    s += "<th>" + str(p_dict["played"]) + "</th>"
+
+    for other_p_name in list_of_players:
+        s += "<td>"
+
+        for rule in rules:
+            if rule == "mp":
+                s += "<br>"
+            if other_p_name in p_dict["games"][rule]:
+                s += '<span style="color: ' + rules[rule][0] + ';">' + str(p_dict["games"][rule][other_p_name]) + "</span>"
+
+            s += " "
+
+        s += "</td>"
+
+    s += "</tr>"
+    print(s)
+
+
+print('</table>')
+
+print()
+print()
+print()
+
+
+
+
+
+print("=======================")
+print("=======================")
+print("=======================")
+print()
+print()
+print()
+print()
+print('<table class="wikitable" border="1" style="text-align: center; width: 100%">')
+
+def get_rule_header():
+    s = "<tr>"
+    for rule in rules:
+        s += "<th></th><th colspan=2>" + rule + "</th>"
+    s += "</tr>"
+    return s
+
+print(get_rule_header())
+
+
+def get_rule_header2():
+    s = "<tr><th>#</th>"
+    first = True
+    for _ in rules:
+        if first:
+            first = False
+        else:
+            s+= "<th></th>"
+        s += "<th>Player</th>"
+        s += "<th>Points</th>"
+    s += "</tr>"
+    return s
+
+print(get_rule_header2())
+
+rankings = {}
+
+for rule in rules:
+    rankings[rule] = []
+    l = rankings[rule]
+
+    for player in list_of_players:
+        l.append(player)
+    
+    def cmp_rule(player_name):
+        return -list_of_players[player_name]["ranking"][rule]["score"], -list_of_players[player_name]["ranking"][rule]["played"]
+    
+    l.sort(key=cmp_rule)
+
+
+for i in range(len(list_of_players)):
+    s = "<tr><th>" + str(i) + "</th>"
+
+    for (k, rule) in enumerate(rules):
+        if k != 0:
+            s += "<th></th>"
+        
+        p_name = rankings[rule][i]
+        s += "<td>" + p_name + "</td><td>" + str(list_of_players[p_name]["ranking"][rule]["score"]) + "</td>"
+    s += "</tr>"
+
+    print(s)
+
+
+print('</table>')
+
+
+print()
+print()
+print()
+print()
+print()
+print()
+
+
+print("=======================")
+print("=======================")
+print("=======================")
+
+
+player_global_ranking = []
+
+for player_name in list_of_players:
+    list_of_players[player_name]["final_rank"] = len(list_of_players) * len(rules)
+
+    for rule in rules:
+        list_of_players[player_name]["final_rank"] -= rankings[rule].index(player_name)
+
+    player_global_ranking.append(player_name)
+
+def global_rank_key(player_name):
+    return -list_of_players[player_name]["final_rank"]
+
+player_global_ranking.sort(key=global_rank_key)
+
+
+
+print()
+print()
+print()
+print()
+print()
+print()
+print('<center><table class="wikitable" border="1" style="text-align: center; width: 40%">')
+print("<tr><th>Position</th><th>Nom</th><th>Points de classement</th></tr>")
+
+
+for (i, player_name) in enumerate(player_global_ranking):
+    s = "<tr><th>" + str(i + 1) + "</th><td>" + player_name + "</td><td>" + str(list_of_players[player_name]["final_rank"]) + "</td></tr>"
+    print(s)
+
+print('</table></center>')
 
 
 
